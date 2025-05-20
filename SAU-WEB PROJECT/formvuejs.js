75b7023991a67906315a31a3fbea5fc2a25af33f
fristale <sahin.bagci@ogr.sakarya.edu.tr>
2025-05-20 09:44:51 +0300

    Vuejs Kısmı
    Vuejs kısmı yapıldı.

---
+++ SAU-WEB PROJECT/formvuejs.js
@@ -0,0 +1,49 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      name: '',
+      surname: '',
+      email: '',
+      message: '',
+      city: '',
+      gender: ''
+    };
+  },
+  methods: {
+    isEmailValid(email) {
+      const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return re.test(email);
+    },
+    sendForm() {
+      if (!this.name.trim()) {
+        alert('Lütfen isminizi giriniz.');
+        return;
+      }
+      if (!this.surname.trim()) {
+        alert('Lütfen soyisminizi giriniz.');
+        return;
+      }
+      if (!this.email.trim()) {
+        alert('Lütfen e-posta adresinizi giriniz.');
+        return;
+      }
+      if (!this.isEmailValid(this.email)) {
+        alert('Lütfen geçerli bir e-posta adresi giriniz.');
+        return;
+      }
+
+      const params = new URLSearchParams({
+        name: this.name,
+        surname: this.surname,
+        email: this.email,
+        message: this.message,
+        city: this.city,
+        gender: this.gender
+      }).toString();
+
+      window.location.href = 'formsonuc.html?' + params;
+    }
+  }
+}).mount('#app');
